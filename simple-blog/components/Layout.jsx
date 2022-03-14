82e1bc91bcaaa0cd86e1fbc2c3f97468a5ae986f
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-03-15 00:01:34 +0300

    add layout

---
+++ simple-blog/components/Layout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../public/blog.png'
+import {Head} from "next/document";
+
+
+const Layout = ({title, description = '', children}) => {
+    return (
+        <>
+            <Head>
+                <title>{title} || Simple Blog NextJs </title>
+                <meta name='description' content={description || 'Простой блог'}/>
+                <meta property='og:description' content={description || 'Простой блог'}/>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <header>
+                <img src='/blog.png' alt=''/>
+                <Link href='/'> <a>Главная</a> </Link>
+            </header>
+
+            <div className="cotainer">
+                {children}
+            </div>
+
+
+            <footer>
+                2021 &copy; Simple Blog NextJs
+            </footer>
+        </>
+    );
+};
+
+export default Layout;
